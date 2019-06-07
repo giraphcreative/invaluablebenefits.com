36fdccdd4df5b22a29aa2e1e5406dda549b6243e
James Pederson <james@jpederson.com>
2019-06-07 01:36:51 -0500

    Fix links and add Google Analytics.

--- js/src/contact.js
+++ js/src/contact.js
@@ -29,6 +29,7 @@ var contact_submit = function( form ) {
 			name: $( form ).find( "input[name=name]" ).val(),
 			email: $( form ).find( "input[name=email]" ).val(),
 			phone: $( form ).find( "input[name=phone]" ).val(),
+			member: $( form ).find( "input[name=member]:checked" ).val(),
 			message: $( form ).find( "textarea" ).val(),
 		},
 		url_params = $.param( field ),
